5c3f392db6e448aa5776e46a0fd1dc7084d2ac4c
“xueyh” <xyh393@126.com>
2018-09-01 17:55:33 +0800

    add optional

--- common/optional.go
+++ common/optional.go
@@ -1,6 +1,41 @@
-/*
- *  @Time : 2018/8/27 下午4:43 
- *  @Author : xueyahui
- *  @File : optional.go
- *  @Software: GoLand
- */
+package common
+
+import "reflect"
+
+type Operator interface {
+	Operator(o interface{})
+}
+
+type Optional struct {
+	T interface{}
+	_valid bool
+	//_value reflect.Value
+}
+
+func NewOptional() Optional {
+	return Optional{_valid:true}
+}
+
+func NewOptional2(i interface{}) Optional {
+	return Optional{i, true}
+}
+
+func (o *Optional) valid() bool{
+	return o._valid
+}
+
+func (o *Optional) Reset(){
+	if o._valid {
+		defer ref()
+	}
+	o._valid = false
+}
+
+func ref() (T interface{}){
+	return reflect.ValueOf(T)
+}
+
+func (o *Optional) Distroy(){
+	o.Reset()
+}
+
