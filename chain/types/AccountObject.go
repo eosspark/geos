5e6c481490911f04016c4d38464220cfaf5e344d
walker <chenwei1204@outlook.com>
2018-09-21 11:42:41 +0800

    modify Hash256

--- chain/types/AccountObject.go
+++ chain/types/AccountObject.go
@@ -6,16 +6,16 @@ import (
 )
 
 type AccountObject struct {
-	ID IdType
-	Name common.AccountName
-	VmType	uint8	//c++ default value 0
-	VmVersion uint8	//c++ default value 0
-	Privileged	bool //c++ default value false
-	LastCodeUpdate	common.TimePoint
-	CodeVersion rlp.Sha256
-	CreationDate	common.BlockTimeStamp
-	Code	string
-	ABI		string
+	ID             IdType
+	Name           common.AccountName
+	VmType         uint8 //c++ default value 0
+	VmVersion      uint8 //c++ default value 0
+	Privileged     bool  //c++ default value false
+	LastCodeUpdate common.TimePoint
+	CodeVersion    rlp.Sha256
+	CreationDate   common.BlockTimeStamp
+	Code           string
+	ABI            string
 }
 
 /*func (ao *AccountObject) SetAbi(Abi){
