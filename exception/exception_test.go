1ee860784252c29aa07541a0026558bafee5e059
yuanchao <541436194@qq.com>
2018-09-28 19:56:46 +0800

    update producer_plugin,mock_chain, create exception
    Signed-off-by: yuanchao <541436194@qq.com>

---
+++ exception/exception_test.go
@@ -0,0 +1,21 @@
+package exception
+
+import (
+	"fmt"
+	"testing"
+)
+
+func assert() {
+	EosAssert(false, ChainException, "wrong chain error : %s", 123)
+
+	defer func() {
+		exp := recover()
+		if exp, ok := recover().(Exception); ok {
+			fmt.Println(exp.code, exp.message)
+		}
+	}()
+}
+
+func TestEosAssert(t *testing.T) {
+	assert()
+}
