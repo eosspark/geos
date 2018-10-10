d30f40264672f46635769fce2a8adbe193d81304
hdongZ <949077032@qq.com>
2018-10-10 14:59:01 +0800

    update resource authority entity

--- entity/permission_usage_object.go
+++ entity/permission_usage_object.go
@@ -3,7 +3,6 @@ package entity
 import "github.com/eosspark/eos-go/common"
 
 type PermissionUsageObject struct {
-	ID                  common.IdType     `storm:"id"`
-	LastUsed            common.TimePoint `json:"last_used"`
-	ByAccountPermission common.Tuple     `storm:"index"`
+	ID                  common.IdType    `multiIndex:"id,increment,byId"`
+	LastUsed            common.TimePoint `multiIndex:"orderedNonUnique"`
 }
