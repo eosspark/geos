1001902e6562126d4a5eabe5cd8d98b2651135b4
hdongZ <949077032@qq.com>
2018-10-11 15:36:12 +0800

    update chain resource authority

--- chain/authorization_manager.go
+++ chain/authorization_manager.go
@@ -1,12 +1,10 @@
 package chain
 
 import (
-	"fmt"
-	"github.com/eosspark/eos-go/chain/types"
-	"github.com/eosspark/eos-go/common"
-	"github.com/eosspark/eos-go/contracts/system"
-	"github.com/eosspark/eos-go/crypto/ecc"
 	"github.com/eosspark/eos-go/database"
+	"github.com/eosspark/eos-go/common"
+	"github.com/eosspark/eos-go/chain/types"
+	"github.com/eosspark/eos-go/crypto/ecc"
 )
 
 var IsActiveAz bool
@@ -19,217 +17,218 @@ type AuthorizationManager struct {
 }
 
 func GetAuthorizationManager() *AuthorizationManager {
-	if !IsActiveAz {
-		azInstance = newAuthorizationManager()
-	}
-	return azInstance
+	return &AuthorizationManager{}
+	//if !IsActiveAz {
+	//	azInstance = newAuthorizationManager()
+	//}
+	//return azInstance
 }
 
-func newAuthorizationManager() *AuthorizationManager {
-	IsActiveAz = true
-	control := GetControllerInstance()
-	db := control.DataBase()
-	return &AuthorizationManager{control: control, db: db}
-}
-
-type PermissionIdType types.IdType
-
-//func (am *AuthorizationManager) AddIndices() {
-//	am.db.Insert(&types.PermissionObject{})
-//	am.db.Insert(&types.PermissionUsageObject{})
-//	am.db.Insert(&types.PermissionLinkObject{})
+//func newAuthorizationManager() *AuthorizationManager {
+//	IsActiveAz = true
+//	control := GetControllerInstance()
+//	db := control.DataBase()
+//	return &AuthorizationManager{control: control, db: db}
 //}
-
-//func (am *AuthorizationManager) InitializeDataBase() {
+//
+//type PermissionIdType types.IdType
+//
+////func (am *AuthorizationManager) AddIndices() {
+////	am.db.Insert(&types.PermissionObject{})
+////	am.db.Insert(&types.PermissionUsageObject{})
+////	am.db.Insert(&types.PermissionLinkObject{})
+////}
+//
+////func (am *AuthorizationManager) InitializeDataBase() {
+////
+////}
+//
+//func (am *AuthorizationManager) CreatePermission(account common.AccountName,
+//	name common.PermissionName,
+//	parent PermissionIdType,
+//	auth types.Authority,
+//	initialCreationTime common.TimePoint,
+//) types.PermissionObject {
+//	creationTime := initialCreationTime
+//	if creationTime == 1 {
+//		creationTime = am.control.PendingBlockTime()
+//	}
+//
+//	permUsage := types.PermissionUsageObject{}
+//	permUsage.LastUsed = creationTime
+//	am.db.Insert(&permUsage)
+//
+//	perm := types.PermissionObject{
+//		UsageId:     permUsage.ID,
+//		Parent:      types.IdType(parent),
+//		Owner:       account,
+//		Name:        name,
+//		LastUpdated: creationTime,
+//		Auth:        am.AuthToShared(auth),
+//	}
+//	am.db.Insert(&perm)
+//	return perm
+//}
+//
+//func (am *AuthorizationManager) ModifyPermission(permission *types.PermissionObject, auth *types.Authority) {
+//	am.db.Update(&permission, func(data interface{}) error {
+//		permission.Auth = am.AuthToShared(*auth)
+//		//permission.LastUpdated = am.control.PendingBlockTime()
+//		return nil
+//	})
+//}
+//
+//func (am *AuthorizationManager) RemovePermission(permission *types.PermissionObject) {
 //
 //}
-
-func (am *AuthorizationManager) CreatePermission(account common.AccountName,
-	name common.PermissionName,
-	parent PermissionIdType,
-	auth types.Authority,
-	initialCreationTime common.TimePoint,
-) types.PermissionObject {
-	creationTime := initialCreationTime
-	if creationTime == 1 {
-		creationTime = am.control.PendingBlockTime()
-	}
-
-	permUsage := types.PermissionUsageObject{}
-	permUsage.LastUsed = creationTime
-	am.db.Insert(&permUsage)
-
-	perm := types.PermissionObject{
-		UsageId:     permUsage.ID,
-		Parent:      types.IdType(parent),
-		Owner:       account,
-		Name:        name,
-		LastUpdated: creationTime,
-		Auth:        am.AuthToShared(auth),
-	}
-	am.db.Insert(&perm)
-	return perm
-}
-
-func (am *AuthorizationManager) ModifyPermission(permission *types.PermissionObject, auth *types.Authority) {
-	am.db.Update(&permission, func(data interface{}) error {
-		permission.Auth = am.AuthToShared(*auth)
-		//permission.LastUpdated = am.control.PendingBlockTime()
-		return nil
-	})
-}
-
-func (am *AuthorizationManager) RemovePermission(permission *types.PermissionObject) {
-
-}
-
-func (am *AuthorizationManager) UpdatePermissionUsage(permission *types.PermissionObject) {
-	puo := types.PermissionUsageObject{}
-	am.db.Find("ID", permission.UsageId, &puo)
-	am.db.Update(&puo, func(data interface{}) error {
-		puo.LastUsed = am.control.PendingBlockTime()
-		return nil
-	})
-}
-
-func (am *AuthorizationManager) GetPermissionLastUsed(permission *types.PermissionObject) common.TimePoint {
-	puo := types.PermissionUsageObject{}
-	am.db.Find("ID", permission.UsageId, &puo)
-	return puo.LastUsed
-}
-
-func (am *AuthorizationManager) FindPermission(level *types.PermissionLevel) *types.PermissionObject {
-	po := types.PermissionObject{}
-	am.db.Find("ByOwner", common.Tuple{level.Actor, level.Permission}, &po)
-	poo := []types.PermissionObject{}
-	am.db.All(&poo)
-	fmt.Println(poo)
-	return &po
-}
-
+//
+//func (am *AuthorizationManager) UpdatePermissionUsage(permission *types.PermissionObject) {
+//	puo := types.PermissionUsageObject{}
+//	am.db.Find("ID", permission.UsageId, &puo)
+//	am.db.Update(&puo, func(data interface{}) error {
+//		puo.LastUsed = am.control.PendingBlockTime()
+//		return nil
+//	})
+//}
+//
+//func (am *AuthorizationManager) GetPermissionLastUsed(permission *types.PermissionObject) common.TimePoint {
+//	puo := types.PermissionUsageObject{}
+//	am.db.Find("ID", permission.UsageId, &puo)
+//	return puo.LastUsed
+//}
+//
+//func (am *AuthorizationManager) FindPermission(level *types.PermissionLevel) *types.PermissionObject {
+//	po := types.PermissionObject{}
+//	am.db.Find("ByOwner", common.Tuple{level.Actor, level.Permission}, &po)
+//	poo := []types.PermissionObject{}
+//	am.db.All(&poo)
+//	fmt.Println(poo)
+//	return &po
+//}
+//
 func (am *AuthorizationManager) GetPermission(level *types.PermissionLevel) types.PermissionObject {
 	po := types.PermissionObject{}
-	am.db.Find("ByOwner", common.Tuple{level.Actor, level.Permission}, &po)
+	//am.db.Find("ByOwner", common.Tuple{level.Actor, level.Permission}, &po)
 	return po
 }
-
-func (am *AuthorizationManager) LookupLinkedPermission(authorizerAccount common.AccountName,
-	scope common.AccountName,
-	actName common.ActionName,
-) common.PermissionName {
-
-	key := common.MakeTuple(authorizerAccount, scope, actName)
-	link := types.PermissionLinkObject{}
-	err := am.db.Find("ByActionName", key, &link)
-	if err != nil {
-		key = common.MakeTuple(authorizerAccount, scope, common.AccountName(common.N("")))
-		err = am.db.Find("ByActionName", key, &link)
-	}
-	if err == nil {
-		return link.RequiredPermission
-	}
-	var pn common.PermissionName
-	return pn
-}
-
-func (am *AuthorizationManager) LookupMinimumPermission(authorizerAccount common.AccountName,
-	scope common.AccountName,
-	actName common.ActionName,
-) common.PermissionName {
-	if scope == common.DefaultConfig.SystemAccountName {
-		//EOS_ASSERT
-	}
-	// if !linkPermission
-	linkedPermission := am.LookupLinkedPermission(authorizerAccount, scope, actName)
-	if linkedPermission == common.PermissionName(common.DefaultConfig.EosioAnyName) {
-		var pn common.PermissionName
-		return pn
-	}
-	return linkedPermission
-}
-
-func (am *AuthorizationManager) CheckUpdateauthAuthorization(update system.UpdateAuth, auths []types.PermissionLevel) {
-	if len(auths) != 1 {
-		fmt.Println("error")
-		return
-	}
-	auth := auths[0]
-	if auth.Actor != update.Account {
-		fmt.Println("error")
-		return
-	}
-	minPermission := am.FindPermission(&types.PermissionLevel{update.Account, update.Permission})
-	if minPermission == nil {
-		permission := am.GetPermission(&types.PermissionLevel{update.Account, update.Permission})
-		minPermission = &permission
-	}
-	if am.GetPermission(&auth).Satisfies(*minPermission) == false {
-		fmt.Println("error")
-		return
-	}
-}
-
-func (am *AuthorizationManager) CheckDeleteauthAuthorization(del system.DeleteAuth, auths []types.PermissionLevel) {
-	if len(auths) != 1 {
-		fmt.Println("error")
-		return
-	}
-	auth := auths[0]
-	if auth.Actor != del.Account {
-		fmt.Println("error")
-		return
-	}
-	minPermission := am.GetPermission(&types.PermissionLevel{del.Account, del.Permission})
-	if am.GetPermission(&auth).Satisfies(minPermission) == false {
-		fmt.Println("error")
-		return
-	}
-}
-
-func (am *AuthorizationManager) CheckLinkauthAuthorization(link system.LinkAuth, auths []types.PermissionLevel) {
-	if len(auths) != 1 {
-		fmt.Println("error")
-		return
-	}
-	auth := auths[0]
-	if auth.Actor != link.Account {
-		fmt.Println("error")
-		return
-	}
-	//TODO
-	linkPermissionName := am.LookupMinimumPermission(link.Account, link.Code, link.Type)
-	if &linkPermissionName == nil {
-		return
-	}
-	//TODO
-}
-
-func (am *AuthorizationManager) CheckUnlinkauthAuthorization(unlink system.UnlinkAuth, auths []types.PermissionLevel) {
-	if len(auths) != 1 {
-		fmt.Println("error")
-		return
-	}
-	auth := auths[0]
-	if auth.Actor != unlink.Account {
-		fmt.Println("error")
-		return
-	}
-	//TODO
-}
-
-func (am *AuthorizationManager) CheckCanceldelayAuthorization(canceldelay system.CancelDelay, auths []types.PermissionLevel) {
-	if len(auths) != 1 {
-		fmt.Println("error")
-		return
-	}
-	auth := auths[0]
-	if am.GetPermission(&auth).Satisfies(am.GetPermission(&canceldelay.CancelingAuth)) == false {
-		fmt.Println("error")
-		return
-	}
-	//TODO
-}
-
+//
+//func (am *AuthorizationManager) LookupLinkedPermission(authorizerAccount common.AccountName,
+//	scope common.AccountName,
+//	actName common.ActionName,
+//) common.PermissionName {
+//
+//	key := common.MakeTuple(authorizerAccount, scope, actName)
+//	link := types.PermissionLinkObject{}
+//	err := am.db.Find("ByActionName", key, &link)
+//	if err != nil {
+//		key = common.MakeTuple(authorizerAccount, scope, common.AccountName(common.N("")))
+//		err = am.db.Find("ByActionName", key, &link)
+//	}
+//	if err == nil {
+//		return link.RequiredPermission
+//	}
+//	var pn common.PermissionName
+//	return pn
+//}
+//
+//func (am *AuthorizationManager) LookupMinimumPermission(authorizerAccount common.AccountName,
+//	scope common.AccountName,
+//	actName common.ActionName,
+//) common.PermissionName {
+//	if scope == common.DefaultConfig.SystemAccountName {
+//		//EOS_ASSERT
+//	}
+//	// if !linkPermission
+//	linkedPermission := am.LookupLinkedPermission(authorizerAccount, scope, actName)
+//	if linkedPermission == common.PermissionName(common.DefaultConfig.EosioAnyName) {
+//		var pn common.PermissionName
+//		return pn
+//	}
+//	return linkedPermission
+//}
+//
+//func (am *AuthorizationManager) CheckUpdateauthAuthorization(update system.UpdateAuth, auths []types.PermissionLevel) {
+//	if len(auths) != 1 {
+//		fmt.Println("error")
+//		return
+//	}
+//	auth := auths[0]
+//	if auth.Actor != update.Account {
+//		fmt.Println("error")
+//		return
+//	}
+//	minPermission := am.FindPermission(&types.PermissionLevel{update.Account, update.Permission})
+//	if minPermission == nil {
+//		permission := am.GetPermission(&types.PermissionLevel{update.Account, update.Permission})
+//		minPermission = &permission
+//	}
+//	if am.GetPermission(&auth).Satisfies(*minPermission) == false {
+//		fmt.Println("error")
+//		return
+//	}
+//}
+//
+//func (am *AuthorizationManager) CheckDeleteauthAuthorization(del system.DeleteAuth, auths []types.PermissionLevel) {
+//	if len(auths) != 1 {
+//		fmt.Println("error")
+//		return
+//	}
+//	auth := auths[0]
+//	if auth.Actor != del.Account {
+//		fmt.Println("error")
+//		return
+//	}
+//	minPermission := am.GetPermission(&types.PermissionLevel{del.Account, del.Permission})
+//	if am.GetPermission(&auth).Satisfies(minPermission) == false {
+//		fmt.Println("error")
+//		return
+//	}
+//}
+//
+//func (am *AuthorizationManager) CheckLinkauthAuthorization(link system.LinkAuth, auths []types.PermissionLevel) {
+//	if len(auths) != 1 {
+//		fmt.Println("error")
+//		return
+//	}
+//	auth := auths[0]
+//	if auth.Actor != link.Account {
+//		fmt.Println("error")
+//		return
+//	}
+//	//TODO
+//	linkPermissionName := am.LookupMinimumPermission(link.Account, link.Code, link.Type)
+//	if &linkPermissionName == nil {
+//		return
+//	}
+//	//TODO
+//}
+//
+//func (am *AuthorizationManager) CheckUnlinkauthAuthorization(unlink system.UnlinkAuth, auths []types.PermissionLevel) {
+//	if len(auths) != 1 {
+//		fmt.Println("error")
+//		return
+//	}
+//	auth := auths[0]
+//	if auth.Actor != unlink.Account {
+//		fmt.Println("error")
+//		return
+//	}
+//	//TODO
+//}
+//
+//func (am *AuthorizationManager) CheckCanceldelayAuthorization(canceldelay system.CancelDelay, auths []types.PermissionLevel) {
+//	if len(auths) != 1 {
+//		fmt.Println("error")
+//		return
+//	}
+//	auth := auths[0]
+//	if am.GetPermission(&auth).Satisfies(am.GetPermission(&canceldelay.CancelingAuth)) == false {
+//		fmt.Println("error")
+//		return
+//	}
+//	//TODO
+//}
+//
 func (am *AuthorizationManager) CheckAuthorization(actions []types.Action,
 	providedKeys []ecc.PublicKey,
 	providedPermission []types.PermissionLevel,
@@ -237,60 +236,60 @@ func (am *AuthorizationManager) CheckAuthorization(actions []types.Action,
 	checkTime func(),
 	allowUnusedKeys bool,
 ) {
-	delayMaxLimit := common.Seconds(int64(am.control.GetGlobalProperties().Configuration.MaxTrxDelay))
-	var effectiveProvidedDelay common.Microseconds
-	if providedDelay >= delayMaxLimit {
-		effectiveProvidedDelay = common.MaxMicroseconds()
-	} else {
-		effectiveProvidedDelay = providedDelay
-	}
-	checker := types.MakeAuthChecker(func(p *types.PermissionLevel) types.SharedAuthority { return am.GetPermission(p).Auth },
-		am.control.GetGlobalProperties().Configuration.MaxAuthorityDepth,
-		providedKeys,
-		providedPermission,
-		effectiveProvidedDelay,
-		checkTime)
-	permissionToSatisfy := make(map[types.PermissionLevel]common.Microseconds)
-	for _, act := range actions {
-		specialCase := false
-		delay := effectiveProvidedDelay
-
-		if act.Account == common.DefaultConfig.SystemAccountName {
-			specialCase = true
-
-			//TODO
-		}
-
-		for _, declaredAuth := range act.Authorization {
-			checkTime()
-			if !specialCase {
-				minPermissionName := am.LookupMinimumPermission(declaredAuth.Actor, act.Account, act.Name)
-				if minPermissionName != common.PermissionName(0) {
-					minPermission := am.GetPermission(&types.PermissionLevel{declaredAuth.Actor, minPermissionName})
-					//EOS_ASSERT
-					if !am.GetPermission(&declaredAuth).Satisfies(minPermission) {
-						fmt.Println("error")
-						return
-					}
-				}
-			}
-			permissionToSatisfy[declaredAuth] = delay
-			//TODO
-		}
-	}
-	for p, q := range permissionToSatisfy {
-		checkTime()
-		if !checker.SatisfiedLoc(&p, q, nil) {
-			fmt.Println("error")
-			return
-		}
-	}
-	if !allowUnusedKeys {
-		if !checker.AllKeysUsed() {
-			fmt.Println("error")
-			return
-		}
-	}
+	//delayMaxLimit := common.Seconds(int64(am.control.GetGlobalProperties().Configuration.MaxTrxDelay))
+	//var effectiveProvidedDelay common.Microseconds
+	//if providedDelay >= delayMaxLimit {
+	//	effectiveProvidedDelay = common.MaxMicroseconds()
+	//} else {
+	//	effectiveProvidedDelay = providedDelay
+	//}
+	//checker := types.MakeAuthChecker(func(p *types.PermissionLevel) types.SharedAuthority { return am.GetPermission(p).Auth },
+	//	am.control.GetGlobalProperties().Configuration.MaxAuthorityDepth,
+	//	providedKeys,
+	//	providedPermission,
+	//	effectiveProvidedDelay,
+	//	checkTime)
+	//permissionToSatisfy := make(map[types.PermissionLevel]common.Microseconds)
+	//for _, act := range actions {
+	//	specialCase := false
+	//	delay := effectiveProvidedDelay
+	//
+	//	if act.Account == common.DefaultConfig.SystemAccountName {
+	//		specialCase = true
+	//
+	//		//TODO
+	//	}
+	//
+	//	for _, declaredAuth := range act.Authorization {
+	//		checkTime()
+	//		if !specialCase {
+	//			minPermissionName := am.LookupMinimumPermission(declaredAuth.Actor, act.Account, act.Name)
+	//			if minPermissionName != common.PermissionName(0) {
+	//				minPermission := am.GetPermission(&types.PermissionLevel{declaredAuth.Actor, minPermissionName})
+	//				//EOS_ASSERT
+	//				if !am.GetPermission(&declaredAuth).Satisfies(minPermission) {
+	//					fmt.Println("error")
+	//					return
+	//				}
+	//			}
+	//		}
+	//		permissionToSatisfy[declaredAuth] = delay
+	//		//TODO
+	//	}
+	//}
+	//for p, q := range permissionToSatisfy {
+	//	checkTime()
+	//	if !checker.SatisfiedLoc(&p, q, nil) {
+	//		fmt.Println("error")
+	//		return
+	//	}
+	//}
+	//if !allowUnusedKeys {
+	//	if !checker.AllKeysUsed() {
+	//		fmt.Println("error")
+	//		return
+	//	}
+	//}
 }
 
 func (am *AuthorizationManager) CheckAuthorization2(account common.AccountName,
@@ -301,26 +300,26 @@ func (am *AuthorizationManager) CheckAuthorization2(account common.AccountName,
 	checkTime func(),
 	allowUnusedKeys bool,
 ) {
-	delayMaxLimit := common.Seconds(int64(am.control.GetGlobalProperties().Configuration.MaxTrxDelay))
-	var effectiveProvidedDelay common.Microseconds
-	if providedDelay >= delayMaxLimit {
-		effectiveProvidedDelay = common.MaxMicroseconds()
-	} else {
-		effectiveProvidedDelay = providedDelay
-	}
-	checker := types.MakeAuthChecker(func(p *types.PermissionLevel) types.SharedAuthority { return am.GetPermission(p).Auth },
-		am.control.GetGlobalProperties().Configuration.MaxAuthorityDepth,
-		providedKeys,
-		providedPermission,
-		effectiveProvidedDelay,
-		checkTime)
-	//TODO
-	if !allowUnusedKeys {
-		if !checker.AllKeysUsed() {
-			fmt.Println("error")
-			return
-		}
-	}
+	//delayMaxLimit := common.Seconds(int64(am.control.GetGlobalProperties().Configuration.MaxTrxDelay))
+	//var effectiveProvidedDelay common.Microseconds
+	//if providedDelay >= delayMaxLimit {
+	//	effectiveProvidedDelay = common.MaxMicroseconds()
+	//} else {
+	//	effectiveProvidedDelay = providedDelay
+	//}
+	//checker := types.MakeAuthChecker(func(p *types.PermissionLevel) types.SharedAuthority { return am.GetPermission(p).Auth },
+	//	am.control.GetGlobalProperties().Configuration.MaxAuthorityDepth,
+	//	providedKeys,
+	//	providedPermission,
+	//	effectiveProvidedDelay,
+	//	checkTime)
+	////TODO
+	//if !allowUnusedKeys {
+	//	if !checker.AllKeysUsed() {
+	//		fmt.Println("error")
+	//		return
+	//	}
+	//}
 }
 
 func (am *AuthorizationManager) GetRequiredKeys(trx types.Transaction,
--- chain/resource_test.go
+++ chain/resource_test.go
@@ -1,85 +1,79 @@
 package chain
 
-import (
-	"fmt"
-	"github.com/eosspark/eos-go/common"
-	"github.com/eosspark/eos-go/database"
-	"testing"
-)
-
-func Test_resourceSetGet(t *testing.T) {
-	var rlm *ResourceLimitsManager
-	db, _ := database.NewDataBase("./", "eos.db", true)
-	defer db.Close()
-	//rlm.AddIndices()
-	rlm.InitializeDatabase()
-	account := common.AccountName(common.N("yc"))
-	rlm.InitializeAccount(account)
-	rlm.SetAccountLimits(account, 123, 456, 789)
-	var r, n, c int64
-	rlm.GetAccountLimits(account, &r, &n, &c)
-	fmt.Println(r, n, c)
-	var rlo []ResourceLimitsObject
-	db.All(&rlo)
-	fmt.Println(rlo)
-	var ruo []ResourceUsageObject
-	db.All(&ruo)
-	fmt.Println(ruo)
-	rlm.ProcessAccountLimitUpdates()
-	//查看账户limits与usage的情况
-	var rlo2 []ResourceLimitsObject
-	db.All(&rlo2)
-	fmt.Println(rlo2)
-	var ruo2 []ResourceUsageObject
-	db.All(&ruo2)
-	fmt.Println(ruo2)
-
-}
-func Test_resourceFuncAdd(t *testing.T) {
-	var rlm *ResourceLimitsManager
-	db, _ := database.NewDataBase("./", "eos.db", true)
-	defer db.Close()
-	rlm.AddIndices()
-	rlm.InitializeDatabase()
-	account1 := common.AccountName(common.N("yc"))
-	//account2 := common.AccountName(common.N("sf"))
-	//account3 := common.AccountName(common.N("hn"))
-	account := []common.AccountName{account1}
-	for _, a := range account {
-		rlm.InitializeAccount(a)
-	}
-
-	//查看账户limits与usage的情况
-	var rlo []ResourceLimitsObject
-	db.All(&rlo)
-	fmt.Println(rlo)
-	var ruo []ResourceUsageObject
-	db.All(&ruo)
-	fmt.Println(ruo)
-
-	rlm.AddTransactionUsage(account, 100000, 100000, 100)
-	var ruo2 []ResourceUsageObject
-	db.All(&ruo2)
-	fmt.Println(ruo2)
-
-	rlm.AddTransactionUsage(account, 100000, 100000, 175600)
-	var ruo3 []ResourceUsageObject
-	db.All(&ruo3)
-	fmt.Println(ruo3)
-
-	rlm.SetAccountLimits(account1, 123, 456, 789)
-
-	rlm.ProcessAccountLimitUpdates()
-	//rlm.ProcessBlockUsage(300)
-	var state ResourceLimitsStateObject
-	db.Find("Id", ResourceLimitsState, &state)
-	fmt.Println(state)
-
-	var arl AccountResourceLimit
-	arl = rlm.GetAccountCpuLimitEx(common.AccountName(common.N("yc")), true)
-	fmt.Println(arl)
-}
-
-func Test_(t *testing.T) {
-
-}
+//
+//func Test_resourceSetGet(t *testing.T) {
+//	var rlm *ResourceLimitsManager
+//	db, _ := database.NewDataBase("./", "eos.db", true)
+//	defer db.Close()
+//	//rlm.AddIndices()
+//	rlm.InitializeDatabase()
+//	account := common.AccountName(common.N("yc"))
+//	rlm.InitializeAccount(account)
+//	rlm.SetAccountLimits(account, 123, 456, 789)
+//	var r, n, c int64
+//	rlm.GetAccountLimits(account, &r, &n, &c)
+//	fmt.Println(r, n, c)
+//	var rlo []ResourceLimitsObject
+//	db.All(&rlo)
+//	fmt.Println(rlo)
+//	var ruo []ResourceUsageObject
+//	db.All(&ruo)
+//	fmt.Println(ruo)
+//	rlm.ProcessAccountLimitUpdates()
+//	//查看账户limits与usage的情况
+//	var rlo2 []ResourceLimitsObject
+//	db.All(&rlo2)
+//	fmt.Println(rlo2)
+//	var ruo2 []ResourceUsageObject
+//	db.All(&ruo2)
+//	fmt.Println(ruo2)
+//
+//}
+//func Test_resourceFuncAdd(t *testing.T) {
+//	var rlm *ResourceLimitsManager
+//	db, _ := database.NewDataBase("./", "eos.db", true)
+//	defer db.Close()
+//	rlm.AddIndices()
+//	rlm.InitializeDatabase()
+//	account1 := common.AccountName(common.N("yc"))
+//	//account2 := common.AccountName(common.N("sf"))
+//	//account3 := common.AccountName(common.N("hn"))
+//	account := []common.AccountName{account1}
+//	for _, a := range account {
+//		rlm.InitializeAccount(a)
+//	}
+//
+//	//查看账户limits与usage的情况
+//	var rlo []ResourceLimitsObject
+//	db.All(&rlo)
+//	fmt.Println(rlo)
+//	var ruo []ResourceUsageObject
+//	db.All(&ruo)
+//	fmt.Println(ruo)
+//
+//	rlm.AddTransactionUsage(account, 100000, 100000, 100)
+//	var ruo2 []ResourceUsageObject
+//	db.All(&ruo2)
+//	fmt.Println(ruo2)
+//
+//	rlm.AddTransactionUsage(account, 100000, 100000, 175600)
+//	var ruo3 []ResourceUsageObject
+//	db.All(&ruo3)
+//	fmt.Println(ruo3)
+//
+//	rlm.SetAccountLimits(account1, 123, 456, 789)
+//
+//	rlm.ProcessAccountLimitUpdates()
+//	//rlm.ProcessBlockUsage(300)
+//	var state ResourceLimitsStateObject
+//	db.Find("Id", ResourceLimitsState, &state)
+//	fmt.Println(state)
+//
+//	var arl AccountResourceLimit
+//	arl = rlm.GetAccountCpuLimitEx(common.AccountName(common.N("yc")), true)
+//	fmt.Println(arl)
+//}
+//
+//func Test_(t *testing.T) {
+//
+//}
