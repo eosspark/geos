6bef0041e0f29323258dfc0ce48ad55c8e44f714
walker <chenwei1204@outlook.com>
2018-10-18 17:57:06 +0800

    add Float128 transferFunc

--- common/arithmetic_types/f128_to_f.go
+++ common/arithmetic_types/f128_to_f.go
@@ -86,22 +86,6 @@ uiZ:
 | pointed to by `zPtr'.  If the NaN is a signaling NaN, the invalid exception
 | is raised.
 *----------------------------------------------------------------------------*/
-//void
-//softfloat_f128UIToCommonNaN(
-//uint_fast64_t uiA64, uint_fast64_t uiA0, struct commonNaN *zPtr )
-//{
-//struct uint128 NaNSig;
-//
-//if ( softfloat_isSigNaNF128UI( uiA64, uiA0 ) ) {
-//softfloat_raiseFlags( softfloat_flag_invalid );
-//}
-//NaNSig = softfloat_shortShiftLeft128( uiA64, uiA0, 16 );
-//zPtr->sign = uiA64>>63;
-//zPtr->v64  = NaNSig.v64;
-//zPtr->v0   = NaNSig.v0;
-//
-//}
-
 func softfloat_f128UIToCommonNaN(uiA64, uiA0 uint64, zPtr *commonNaN) {
 	var NaNSig Uint128
 	if softfloat_isSigNaNF128UI(uiA64, uiA0) {
@@ -125,8 +109,6 @@ func softfloat_f128UIToCommonNaN(uiA64, uiA0 uint64, zPtr *commonNaN) {
 | point signaling NaN.
 | Note:  This macro evaluates its arguments more than once.
 *----------------------------------------------------------------------------*/
-//#define softfloat_isSigNaNF128UI( uiA64, uiA0 ) ((((uiA64) & UINT64_C( 0x7FFF800000000000 )) == UINT64_C( 0x7FFF000000000000 )) && ((uiA0) || ((uiA64) & UINT64_C( 0x00007FFFFFFFFFFF ))))
-
 func softfloat_isSigNaNF128UI(uiA64, uiA0 uint64) bool {
 	return (uiA64&uint64(0x7FFF800000000000)) == uint64(0x7FFF000000000000) && (uiA0 != 0 || (uiA64&uint64(0x00007FFFFFFFFFFF) != 0))
 }
@@ -135,13 +117,6 @@ func softfloat_isSigNaNF128UI(uiA64, uiA0 uint64) bool {
 | Converts the common NaN pointed to by `aPtr' into a 32-bit floating-point
 | NaN, and returns the bit pattern of this value as an unsigned integer.
 *----------------------------------------------------------------------------*/
-//uint_fast32_t softfloat_commonNaNToF32UI( const struct commonNaN *aPtr )
-//{
-//
-//return (uint_fast32_t) aPtr->sign<<31 | 0x7FC00000 | aPtr->v64>>41;
-//
-//}
-
 func softfloat_commonNaNToF32UI(aPtr *commonNaN) uint32 {
 	if aPtr.sign {
 		return uint32(1)<<31 | uint32(0x7FC00000) | uint32(aPtr.v64>>41)
@@ -152,10 +127,25 @@ func softfloat_commonNaNToF32UI(aPtr *commonNaN) uint32 {
 
 }
 
+//#define signF32UI( a ) ((bool) ((uint32_t) (a)>>31))
+//#define expF32UI( a ) ((int_fast16_t) ((a)>>23) & 0xFF)
+//#define fracF32UI( a ) ((a) & 0x007FFFFF)
 //#define packToF32UI( sign, exp, sig ) (((uint32_t) (sign)<<31) + ((uint32_t) (exp)<<23) + (sig))
 
-func packToF32UI(sign bool, exp, sig uint32) uint32 {
+func signF32UI(a uint32) bool {
+	if a>>31 == 0 {
+		return false
+	}
+	return true
+}
+func expF32UI(a uint32) int16 {
+	return int16(a>>23) & 0xFF
+}
+func fracF32UI(a uint32) uint32 {
+	return a & 0x007FFFFF
+}
 
+func packToF32UI(sign bool, exp, sig uint32) uint32 {
 	if sign {
 		return uint32(1)<<31 + uint32(exp<<23) + sig
 	} else {
@@ -163,9 +153,6 @@ func packToF32UI(sign bool, exp, sig uint32) uint32 {
 	}
 
 }
-
-//uint64_t softfloat_shortShiftRightJam64( uint64_t a, uint_fast8_t dist )
-//{ return a>>dist | ((a & (((uint_fast64_t) 1<<dist) - 1)) != 0); }
 
 func softfloat_shortShiftRightJam64(a uint64, dist uint8) uint64 {
 	if (a & ((uint64(1) << dist) - 1)) != 0 {
@@ -204,7 +191,7 @@ func softfloat_roundPackToF32(sign bool, exp int16, sig uint32) Float32 {
 		if exp < 0 {
 			/*----------------------------------------------------------------
 			*----------------------------------------------------------------*/
-			isTiny = (softfloat_detectTininess == softfloat_tininess_beforeRounding) || (exp < -1) || (sig+uint32(roundIncrement) < 0x80000000)
+			isTiny = (softfloat_detectTininess == softfloat_tininess_beforeRounding) || (exp < -1) || (sig+uint32(roundIncrement) < uint32(0x80000000))
 			sig = softfloat_shiftRightJam32(sig, uint16(-exp))
 			exp = 0
 			roundBits = uint8(sig & 0x7F)
@@ -227,11 +214,6 @@ func softfloat_roundPackToF32(sign bool, exp int16, sig uint32) Float32 {
 	}
 
 	sig = (sig + uint32(roundIncrement)) >> 7
-	//if  roundBits !=0 {
-	//	//softfloat_exceptionFlags |= softfloat_flag_inexact
-	//	#ifdef SOFTFLOAT_ROUND_ODD
-	//}
-	//sig &= ~(uint_fast32_t) (! (roundBits ^ 0x40) & roundNearEven);
 	if roundBits^0x40 != 0 {
 		sig &= ^uint32(0)
 	} else {
@@ -252,12 +234,6 @@ uiZ:
 
 }
 
-//INLINE uint32_t softfloat_shiftRightJam32( uint32_t a, uint_fast16_t dist )
-//{
-//return
-//(dist < 31) ? a>>dist | ((uint32_t) (a<<(-dist & 31)) != 0) : (a != 0);
-//}
-
 func softfloat_shiftRightJam32(a uint32, dist uint16) uint32 {
 	if dist < 31 {
 		if a<<(-dist&31) != 0 {
@@ -271,5 +247,169 @@ func softfloat_shiftRightJam32(a uint32, dist uint16) uint32 {
 	} else {
 		return 0
 	}
+
+}
+
+func F128ToF64(a Float128) Float64 {
+	var uA Float128
+	var uiA64, uiA0 uint64
+	var sign bool
+	var exp int32
+	var frac64, frac0 uint64
+	var commonNaN commonNaN
+	var uiZ uint64
+	var frac128 Uint128
+
+	uA = a
+	uiA64 = uA.High
+	uiA0 = uA.Low
+	sign = signF128Ui64(uiA64)
+	exp = expF128Ui64(uiA64)
+	frac64 = fracF128Ui64(uiA64)
+	frac0 = uiA0
+
+	if exp == 0x7FFF {
+		if (frac64 | frac0) != 0 {
+			softfloat_f128UIToCommonNaN(uiA64, uiA0, &commonNaN)
+			uiZ = softfloat_commonNaNToF64UI(&commonNaN)
+		} else {
+			uiZ = packToF64UI(sign, 0x7FF, 0)
+		}
+		goto uiZ
+	}
+
+	frac128 = softfloat_shortShiftLeft128(frac64, frac0, 14)
+	if frac128.Low != 0 {
+		frac64 = frac128.High | 1
+	} else {
+		frac64 = frac128.High
+	}
+
+	if int64(exp)|int64(frac64) == 0 {
+		uiZ = packToF64UI(sign, 0, 0)
+		goto uiZ
+	}
+
+	exp -= 0x3C01
+	if unsafe.Sizeof(int16(0)) < unsafe.Sizeof(int32(0)) {
+		if exp < -0x1000 {
+			exp = -0x1000
+		}
+	}
+
+	return softfloat_roundPackToF64(sign, int16(exp), frac64|uint64(0x4000000000000000))
+
+uiZ:
+	return Float64(uiZ)
+
+}
+
+/*----------------------------------------------------------------------------
+| Converts the common NaN pointed to by `aPtr' into a 64-bit floating-point
+| NaN, and returns the bit pattern of this value as an unsigned integer.
+*----------------------------------------------------------------------------*/
+func softfloat_commonNaNToF64UI(aPtr *commonNaN) uint64 {
+	if aPtr.sign {
+		return uint64(1)<<63 | uint64(0x7FF8000000000000) | aPtr.v64>>12
+	}
+	return uint64(0x7FF8000000000000) | aPtr.v64>>12
+}
+
+//#define signF64UI( a ) ((bool) ((uint64_t) (a)>>63))
+//#define expF64UI( a ) ((int_fast16_t) ((a)>>52) & 0x7FF)
+//#define fracF64UI( a ) ((a) & UINT64_C( 0x000FFFFFFFFFFFFF ))
+//#define packToF64UI( sign, exp, sig ) ((uint64_t) (((uint_fast64_t) (sign)<<63) + ((uint_fast64_t) (exp)<<52) + (sig)))
+
+func signF64UI(a uint64) bool {
+	if a>>63 == 0 {
+		return false
+	}
+	return true
+}
+
+func expF64UI(a uint64) int16 {
+	return int16(a>>52) & 0x7FF
+}
+
+func fracF64UI(a uint64) uint64 {
+	return a & uint64(0x000FFFFFFFFFFFFF)
+}
+
+func packToF64UI(sign bool, exp, sig uint64) uint64 {
+	if sign {
+		return uint64(1)<<63 + exp<<52 + sig
+	}
+	return exp<<52 + sig //return uint64(0)<< 63 + exp <<52 + sig
+}
+
+func softfloat_roundPackToF64(sign bool, exp int16, sig uint64) Float64 {
+	var roundingMode uint8
+	var roundNearEven bool
+	var roundIncrement, roundBits uint16
+	var isTiny bool
+	var uiZ uint64
+
+	roundingMode = softfloat_roundingMode
+	roundNearEven = roundingMode == softfloat_round_near_even
+	roundIncrement = 0x200
+	if !(roundNearEven && (roundingMode != softfloat_round_near_maxMag)) {
+		var mode uint8
+		if sign {
+			mode = softfloat_round_min
+		} else {
+			mode = softfloat_round_max
+		}
+		if roundingMode == mode {
+			roundIncrement = 0x3FF
+		} else {
+			roundIncrement = 0
+		}
+	}
+	roundBits = uint16(sig & 0x3FF)
+
+	if 0x7FD <= uint16(exp) {
+		if exp < 0 {
+			/*----------------------------------------------------------------
+			*----------------------------------------------------------------*/
+			isTiny = (softfloat_detectTininess == softfloat_tininess_beforeRounding) || (exp < -1) || (sig+uint64(roundIncrement) < uint64(0x8000000000000000))
+			sig = softfloat_shiftRightJam64(sig, uint32(-exp))
+			exp = 0
+			roundBits = uint16(sig & 0x3FF)
+			if isTiny && roundBits != 0 {
+				softfloat_raiseFlags(softfloat_flag_underflow)
+			}
+		} else if (0x7FD < exp) || (uint64(0x8000000000000000) <= sig+uint64(roundIncrement)) {
+			/*----------------------------------------------------------------
+			*----------------------------------------------------------------*/
+			//softfloat_raiseFlags(softfloat_flag_overflow | softfloat_flag_inexact)
+
+			if roundIncrement != 0 {
+				uiZ = packToF64UI(sign, 0xFF, 0)
+			} else {
+				uiZ = packToF64UI(sign, 0xFF, 0) - 1
+			}
+			goto uiZ
+		}
+	}
+
+	sig = (sig + uint64(roundIncrement)) >> 10
+	if roundBits^0x200 != 0 {
+		sig &= ^uint64(0)
+	} else {
+		if roundNearEven {
+			sig &= ^uint64(1)
+		} else {
+			sig &= ^uint64(0)
+		}
+	}
+
+	if sig == 0 {
+		exp = 0
+	}
+
+	uiZ = packToF64UI(sign, uint64(exp), sig)
+
+uiZ:
+	return Float64(uiZ)
 
 }
--- common/arithmetic_types/float128_test.go
+++ common/arithmetic_types/float128_test.go
@@ -64,12 +64,12 @@ func TestF128ToF32(t *testing.T) {
 
 	minusF128 := Float128{High: 13836623759840116736, Low: 0}
 	b := F128ToF32(minusF128)
-	assert.Equal(t, 3267887104, int(b))
+	assert.Equal(t, Float32(3267887104), b)
 
 	int60 := int64(1) << 60
 	f12860 := I64ToF128(int60)
 	c := F128ToF32(f12860)
-	assert.Equal(t, 1568669696, int(c))
+	assert.Equal(t, Float32(1568669696), c)
 }
 
 func TestF128ToF64(t *testing.T) {
@@ -90,4 +90,52 @@ func TestF128ToF64(t *testing.T) {
 	d := F128ToF64(test)
 	fmt.Println(d)
 	assert.Equal(t, Float64(4894998396442247172), d)
+
+}
+
+func TestF32ToF128(t *testing.T) {
+	plusF128 := Float128{High: 4613251722985340928, Low: 0}
+	a := F128ToF32(plusF128)
+	assert.Equal(t, Float32(1120403456), a)
+	f128 := F32ToF128(a)
+	assert.Equal(t, plusF128, f128)
+
+	minusF128 := Float128{High: 13836623759840116736, Low: 0}
+	b := F128ToF32(minusF128)
+	assert.Equal(t, Float32(3267887104), b)
+	f128minus := F32ToF128(b)
+	assert.Equal(t, minusF128, f128minus)
+
+	int60 := int64(1) << 60
+	f12860 := I64ToF128(int60)
+	c := F128ToF32(f12860)
+	f60 := F32ToF128(c)
+	assert.Equal(t, f12860, f60)
+}
+
+func TestF64ToF128(t *testing.T) {
+	plusF128 := Float128{High: 4613251722985340928, Low: 0} //100
+	a := F128ToF64(plusF128)
+	assert.Equal(t, Float64(4636737291354636288), a)
+
+	f128plus := F64ToF128(a)
+	assert.Equal(t, plusF128, f128plus)
+
+	minusF128 := Float128{High: 13836623759840116736, Low: 0}
+	b := F128ToF64(minusF128)
+	assert.Equal(t, Float64(13860109328209412096), b)
+	f128minus := F64ToF128(b)
+	assert.Equal(t, minusF128, f128minus)
+
+	int60 := int64(1) << 60
+	f12860 := I64ToF128(int60)
+	c := F128ToF64(f12860)
+	assert.Equal(t, Float64(4877398396442247168), c)
+
+	test := Float128{High: 4629393042053316608, Low: 4629393042053316608}
+	d := F128ToF64(test)
+	assert.Equal(t, Float64(4894998396442247172), d)
+	f128d := F64ToF128(d)
+	fmt.Println(f128d)
+	assert.Equal(t, Float128{High: 4629393042053316608, Low: 4611686018427387904}, f128d)
 }
