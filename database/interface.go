8a173687a4ce5b2bd77407beb1b2f7d5d2d8ae81
garytone <654613310@qq.com>
2018-10-19 18:57:12 +0800

    add undo function

--- database/interface.go
+++ database/interface.go
@@ -2,6 +2,8 @@ package database
 
 type DataBase interface {
 
+	Close()
+
 	Insert(data interface{}) error
 
 	Find(fieldName string, data interface{}, out interface{}) error
@@ -12,9 +14,21 @@ type DataBase interface {
 
 	Remove(data interface{}) error
 
+	Undo()
+
+	UndoAll()
+
+	StartSession() *Session
+
+	Commit(revision int64)
+
+	SetRevision(revision int64)
+
+	Revision() int64
+
 	lowerBound(key,value,typeName []byte,data interface{},greater bool) (*DbIterator,error)
 
 	upperBound(key,value,typeName []byte,data interface{},greater bool) (*DbIterator,error)
 
-	Close()
+	squash()
 }
