f8348955aa96f9facf7fcebbd54a37a3d9caf5a6
hdongZ <949077032@qq.com>
2018-11-06 09:22:57 +0800

    new chain::auth_test && chain::tester by huidong on 11.6

---
+++ chain/auth_test.go
@@ -0,0 +1,18 @@
+package chain
+
+import (
+	"testing"
+)
+
+func initializeAuth() *AuthorizationManager{
+	control := GetControllerInstance()
+	am := control.Authorization
+	return am
+}
+
+func TestMissingSigs(t *testing.T){
+	am := initializeAuth()
+	createNewAccount(am.control, "Alice")
+	//produceBlock
+
+}
