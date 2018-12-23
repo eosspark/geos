aeee4082cd57e3efa778ccc6c9e30641932adeb8
xiaoyu1998 <1982139199@qq.com>
2018-12-23 20:31:17 +0800

    unittests:add TestF32F64overflow

--- unittests/wasm_test.go
+++ unittests/wasm_test.go
@@ -1,6 +1,7 @@
 package unittests
 
 import (
+	"fmt"
 	"github.com/eosspark/eos-go/chain"
 	"github.com/eosspark/eos-go/chain/types"
 	"github.com/eosspark/eos-go/common"
@@ -9,6 +10,8 @@ import (
 	"github.com/eosspark/eos-go/exception/try"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
 )
@@ -368,5 +371,97 @@ func TestFloat64Bitwise(t *testing.T) {
 		//trxId := trx.ID()
 		//assert.Equal(t, b.ChainHasTransaction(&trxId), true)
 		b.close()
+	})
+}
+
+//var i32_overflow_wast string = "(module
+//  (import \"env\" \"require_auth\" (func $require_auth (param i64)))
+//  (import "env" "eosio_assert" (func $eosio_assert (param i32 i32)))
+//   (table 0 anyfunc)
+//   (memory $0 1)
+//   (export \"apply\" (func $apply))
+//   (func $i32_trunc_s_f32 (param $0 f32) (result i32) (i32.trunc_s/f32 (get_local $0)))
+//   (func $i32_trunc_u_f32 (param $0 f32) (result i32) (i32.trunc_u/f32 (get_local $0)))
+//   (func $i32_trunc_s_f64 (param $0 f64) (result i32) (i32.trunc_s/f64 (get_local $0)))
+//   (func $i32_trunc_u_f64 (param $0 f64) (result i32) (i32.trunc_u/f64 (get_local $0)))
+//   (func $test (param $0 i32))
+//   (func $apply (param $0 i64)(param $1 i64)(param $2 i64)
+//    (call $test (call $%s (%s)))
+//))"
+
+func wast2wasm(wast []uint8) []uint8 {
+	wastTmp := "wast_tmp.wast"
+	wasmTmp := "wast_tmp.wasm"
+	os.Remove(wastTmp)
+	os.Remove(wasmTmp)
+	ioutil.WriteFile(wastTmp, wast, os.ModePerm)
+	cmd := exec.Command("./test_contracts/wat2wasm", wastTmp)
+	cmd.Run()
+	code, _ := ioutil.ReadFile(wasmTmp)
+	os.Remove(wastTmp)
+	os.Remove(wasmTmp)
+	return code
+}
+
+func TestF32F64overflow(t *testing.T) {
+	name := "test_contracts/i32_overflow.wast"
+	t.Run(filepath.Base(name), func(t *testing.T) {
+		code, err := ioutil.ReadFile(name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		i32_overflow_wast := string(code)
+
+		f_tests := common.N("f_tests")
+		b := newBaseTester(true, chain.SPECULATIVE)
+
+		var count uint64 = 0
+		check := func(wastTemplate string, op string, param string) bool {
+			count += 16
+			tester := common.AccountName(uint64(f_tests) + count)
+			b.CreateAccounts([]common.AccountName{tester}, false, true)
+			b.ProduceBlocks(1, false)
+
+			wast := fmt.Sprintf(wastTemplate, op, param)
+			wasm := wast2wasm([]byte(wast))
+			b.SetCode(tester, wasm, nil)
+			b.ProduceBlocks(10, false)
+
+			trx := types.SignedTransaction{}
+			act := types.Action{
+				Account:       tester,
+				Name:          common.N(""),
+				Authorization: []types.PermissionLevel{{tester, common.DefaultConfig.ActiveName}}}
+			trx.Actions = append(trx.Actions, &act)
+			b.SetTransactionHeaders(&trx.Transaction, b.DefaultExpirationDelta, 0)
+
+			privKey := b.getPrivateKey(tester, "active")
+			chainId := b.Control.GetChainId()
+			trx.Sign(&privKey, &chainId)
+
+			returning := false
+			try.Try(func() {
+				b.PushTransaction(&trx, common.MaxTimePoint(), b.DefaultBilledCpuTimeUs)
+				b.ProduceBlocks(1, false)
+				trxId := trx.ID()
+				assert.Equal(t, b.ChainHasTransaction(&trxId), true)
+				returning = true
+			}).Catch(func(e exception.Exception) {
+			}).End()
+			return returning
+		}
+
+		//// float32 => int32
+		// 2^31
+		assert.Equal(t, check(i32_overflow_wast, "i32_trunc_s_f32", "f32.const 2147483648"), false)
+		// the maximum value below 2^31 representable in IEEE float32
+		assert.Equal(t, check(i32_overflow_wast, "i32_trunc_s_f32", "f32.const 2147483520"), true)
+		// -2^31
+		assert.Equal(t, check(i32_overflow_wast, "i32_trunc_s_f32", "f32.const -2147483648"), true)
+		// the maximum value below -2^31 in IEEE float32
+		assert.Equal(t, check(i32_overflow_wast, "i32_trunc_s_f32", "f32.const -2147483904"), true)
+
+		b.close()
+
 	})
 }
